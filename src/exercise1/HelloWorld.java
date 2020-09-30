ca95f84b883812593b8f336fab56c8ec3e654a97
Stefan Georgiev <stefan.georgiev91@gmail.com>
2020-09-30 23:08:21 +0300

    Add files via upload

--- src/exercise1/HelloWorld.java
+++ src/exercise1/HelloWorld.java
@@ -1,9 +1,6 @@
 package exercise1;
-
 public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+  public static void main (String[] args) {
+        System.out.println("HelloWorld!");
     }
-
 }
