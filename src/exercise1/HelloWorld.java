89779053b18f7133a116f46244f39e7a4f80b211
Stefan Georgiev <stefan.georgiev91@gmail.com>
2020-09-30 23:09:11 +0300

    Update HelloWorld.java

--- src/exercise1/HelloWorld.java
+++ src/exercise1/HelloWorld.java
@@ -1,6 +1,6 @@
 package exercise1;
 public class HelloWorld {
   public static void main (String[] args) {
-        System.out.println("HelloWorld!");
+        System.out.println("Hello World!");
     }
 }
